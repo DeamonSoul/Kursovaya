c2495dfa20854b0f9434a47f7c3e788846f5d415
CobaChka9033 <97450992+CobaChka9033@users.noreply.github.com>
2022-01-10 20:21:23 +0300

    beta 2\0

--- 1.cs
+++ 1.cs
@@ -14,10 +14,13 @@ namespace NetConsoleApp
             int smtpPort = 465;
             string login = "islammusin@mail.ru";
             string pass = "TJRZG";
+            SmtpClient client = new SmtpClient(smtpHost, smtpPort);
+            client.Credentials = new NetworkCredential(login, pass);
             string from = "islammusin@mail.ru";
             string to = "islammusin.musin@yandex.ru";
             string subject = "Письмо от C Sharp";
             string body = "Привет! \n\n\n Это тестовое письмо";
+            MailMessage mess = new MailMessage(from, to, subject, body);
         }
     }
 }
