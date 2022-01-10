fbc0c25e9ecbcaad3d7ec2dd6617ff3dcf044bdd
Ruslan <rus.korepanov.02@mail.ru>
2022-01-10 23:37:26 +0400

    this code contains an email login check, a password and a person's full name

--- WindowsFormsApp2/WindowsFormsApp2/one.cs
+++ WindowsFormsApp2/WindowsFormsApp2/one.cs
@@ -8,7 +8,45 @@ namespace WindowsFormsApp2
 {
     internal static class InputAnalysis
     {
-        
+        public static bool CheckLogin(string login)
+        {
+            if (login.Length != 12)
+                return false;
+
+            foreach (char c in login)
+            {
+                if (!(c >= '0' && c <= '9'))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static bool CheckPassword(string password)
+        {
+
+            foreach (char c in password)
+            {
+                if (c == ' ')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool CheckEmail(string email)
+        {
+            string[] strs = email.Split(new char[] { '@' });
+
+            if (strs.Length != 2)
+                return false;
+            else
+                return true;
+        }
+
+        public static bool CheckFio(string name)
         {
             const string russianLetters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
             return text.Any(letter => russianLetters.Contains(letter));
