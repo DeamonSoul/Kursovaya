38c6a10c289386c1c4d9f8ca09c91fb16c794a33
CobaChka9033 <97450992+CobaChka9033@users.noreply.github.com>
2022-01-10 19:59:20 +0300

    beta 1/0

---
+++ 1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace NetConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string smtpHost = "smtp.mail.ru";
+            int smtpPort = 465;
+            string login = "islammusin@mail.ru";
+            string pass = "TJRZG";
+            string from = "islammusin@mail.ru";
+            string to = "islammusin.musin@yandex.ru";
+            string subject = "Письмо от C Sharp";
+            string body = "Привет! \n\n\n Это тестовое письмо";
+        }
+    }
+}
