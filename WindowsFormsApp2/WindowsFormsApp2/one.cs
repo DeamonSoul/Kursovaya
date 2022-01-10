98a15e190618b4833153a2f1e94e473385d173bc
Ruslan <rus.korepanov.02@mail.ru>
2022-01-10 22:55:52 +0400

    this describes user data

--- WindowsFormsApp2/WindowsFormsApp2/one.cs
+++ WindowsFormsApp2/WindowsFormsApp2/one.cs
@@ -8,40 +8,21 @@ namespace WindowsFormsApp2
 {
     internal static class InputAnalysis
     {
-        public static bool CheckLogin(string login)
+        
         {
-            if (login.Length != 12)
-                return false;
-
-            foreach (char c in login)
+            const string russianLetters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+            return text.Any(letter => russianLetters.Contains(letter));
+            var count;
+            string str = new char[];
+            for (int i = 0; str[i]; i++)
             {
-                if (!(c >= '0' && c <= '9'))
-                    return false;
+                if (str[i] == ' ')
+                    count++;
             }
-
-            return true;
-        }
-
-        public static bool CheckPassword(string password)
-        {
-
-            foreach (char c in password)
-            {
-                if (c == ' ')
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        public static bool CheckEmail(string email)
-        {
-            string[] strs = email.Split(new char[] { '@' });
-
-            if (strs.Length != 2)
-                return false;
-            else
+            if (count == 2)
                 return true;
+            {
+
+            }
         }
+    }
