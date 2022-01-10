2493e9d2b4205f534b5e8746e42d05ce2073d06f
DeamonSoul <dimitry.vorontcov@mail.ru>
2022-01-10 22:11:05 +0400

    Добавил тело функции AskForUser

--- WindowsFormsApp2/WindowsFormsApp2/DataBaseRequest.cs
+++ WindowsFormsApp2/WindowsFormsApp2/DataBaseRequest.cs
@@ -14,17 +14,11 @@ namespace WindowsFormsApp2
     internal static class DataBaseRequest
     {
 
-        /// <returns></returns>
+        
         public static DataTable AskForData(string commandStr, params string[] args)
         {
-            DataBaseInit DB = new DataBaseInit();
             DataTable table = new DataTable();
-            MySqlDataAdapter adapter = new MySqlDataAdapter();
-            MySqlCommand command = new MySqlCommand(commandStr, DB.GetConnector());
-            for (int i = 0; i < args.Length; i++)
-                command.Parameters.Add("@P" + i.ToString(), MySqlDbType.VarChar).Value = args[i];
-            adapter.SelectCommand = command;
-            adapter.Fill(table);
+            
             return table;
         }
     }
