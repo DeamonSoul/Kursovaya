f15e5fed558b468963232561fc2588ec3ee20195
DeamonSoul <dimitry.vorontcov@mail.ru>
2022-01-10 22:34:41 +0400

    Дописал функцию WriteNewUser

--- WindowsFormsApp2/WindowsFormsApp2/DataBaseRequest.cs
+++ WindowsFormsApp2/WindowsFormsApp2/DataBaseRequest.cs
@@ -38,7 +38,29 @@ namespace WindowsFormsApp2
 
         public static void WriteNewUser(string inn, string name, string email, string password)
         {
-            
+            DataBaseInit DB = new DataBaseInit();
+            MySqlCommand command = new MySqlCommand("INSERT INTO `usersdata` (`Inn`, `Name`, `Email`, `Password`, `Bonus`, `Income`, `Credit`, `Ask`, `Answer`) VALUES (@P0, @P1, @P2, @P3, '10000', '0', '0', '', '')", DB.GetConnector());
+
+            command.Parameters.Add("@P0", MySqlDbType.VarChar).Value = inn;
+            command.Parameters.Add("@P1", MySqlDbType.VarChar).Value = name;
+            command.Parameters.Add("@P2", MySqlDbType.VarChar).Value = email;
+            command.Parameters.Add("@P3", MySqlDbType.VarChar).Value = password;
+
+            DB.OpenConnection();
+
+            if (command.ExecuteNonQuery() == 1)
+                MessageBox.Show("Аккаунт был создан");
+            else
+            {
+                MessageBox.Show("Аккаунт не был создан");
+                return;
+            }
+
+            command = new MySqlCommand("CREATE TABLE `users`.`list" + inn.ToString() + "` ( `Id` INT(5) UNSIGNED NOT NULL AUTO_INCREMENT , `CustomerInn` VARCHAR(12) NOT NULL , `CustomerType` VARCHAR(1) NOT NULL , `ProductName` VARCHAR(50) NOT NULL , `Price` VARCHAR(15) NOT NULL, PRIMARY KEY (`ID`) ) ENGINE = InnoDB", DB.GetConnector());
+
+            command.ExecuteNonQuery();
+
+            DB.CloseConnection();
         }
     }
 }
